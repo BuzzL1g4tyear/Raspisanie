ce13927ba460825a4f57b2b1be0b393960d706ad
BuzzL1g4tyear <70737193+BuzzL1g4tyear@users.noreply.github.com>
2021-05-10 14:38:55 +0300

    Upd 10.05(Single_Chat_Fr)

---
+++ app/src/main/java/com/example/Ras/UI/messUI/SingleChatFragment.kt
@@ -0,0 +1,9 @@
+package com.example.Ras.UI.messUI
+
+import com.example.Ras.R
+
+class SingleChatFragment : BaseFragment(R.layout.fragment_single_chat) {
+    override fun onResume() {
+        super.onResume()
+    }
+}
