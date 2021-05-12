1e58fe460129f44d5f6e2d6a133b35c03e16c244
BuzzL1g4tyear <70737193+BuzzL1g4tyear@users.noreply.github.com>
2021-05-12 11:47:01 +0300

    Upd 10.05(Single_Chat_Fr)

--- app/src/main/java/com/example/Ras/RegisterFragment.kt
+++ app/src/main/java/com/example/Ras/RegisterFragment.kt
@@ -1,6 +1,7 @@
 package com.example.Ras
 
 import android.util.Log
+import android.widget.ArrayAdapter
 import com.example.Ras.UI.messUI.BaseFragment
 import com.example.Ras.Utils.*
 import kotlinx.android.synthetic.main.fragment_register.*
@@ -8,30 +9,37 @@ import kotlinx.android.synthetic.main.fragment_register.*
 class RegisterFragment() : BaseFragment(R.layout.fragment_register) {
     override fun onStart() {
         super.onStart()
+        setSpinner()
         (activity as MessengerActivity).title = getString(R.string.addNewUser)
         button.setOnClickListener {
             creteUserWithEmail()
         }
     }
 
-    // TODO: 06.05.2021 красивый вью
+    private fun setSpinner() {
+        val status = resources.getStringArray(R.array.status_array)
+        val adapter = ArrayAdapter(requireContext(), R.layout.dropdown_status, status)
+        status_spinner.setAdapter(adapter)
+    }
+
+    // TODO: 06.05.2021 красивый вью, group
     private fun creteUserWithEmail() {
         val email: String = ed1.text.toString()
         val password: String = ed2.text.toString()
         val status: String = ed3.text.toString()
         AUTH.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(requireActivity()) { task ->
-                    if (task.isSuccessful) {
-                        val uId = AUTH.currentUser?.uid.toString()
-                        val dataMap = mutableMapOf<String, Any>()
-                        dataMap[CHILD_ID] = uId
-                        dataMap[CHILD_EMAIL] = email
-                        dataMap[CHILD_FULLNAME] = email
-                        dataMap[CHILD_STATUS] = status
-                        REF_DATABASE.child(NODE_USERS).child(uId).updateChildren(dataMap)
-                    } else {
-                        Log.d("MyLog", "Boom ${task.exception?.message.toString()} ")
-                    }
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    val uId = AUTH.currentUser?.uid.toString()
+                    val dataMap = mutableMapOf<String, Any>()
+                    dataMap[CHILD_ID] = uId
+                    dataMap[CHILD_EMAIL] = email
+                    dataMap[CHILD_FULLNAME] = email
+                    dataMap[CHILD_STATUS] = status
+                    REF_DATABASE.child(NODE_USERS).child(uId).updateChildren(dataMap)
+                } else {
+                    Log.d("MyLog", "Boom ${task.exception?.message.toString()} ")
                 }
+            }
     }
 }
