85cd2c7d782bc628bb93af7d0921e8fc94686a57
BuzzL1g4tyear <70737193+BuzzL1g4tyear@users.noreply.github.com>
2021-05-17 14:34:49 +0300

    17.05

--- app/src/main/java/com/example/Ras/UI/messUI/SingleChatFragment.kt
+++ app/src/main/java/com/example/Ras/UI/messUI/SingleChatFragment.kt
@@ -2,22 +2,21 @@ package com.example.Ras.UI.messUI
 
 import android.view.View
 import com.example.Ras.R
-import com.example.Ras.Utils.AppValueEventListener
-import com.example.Ras.Utils.MESS_ACTIVITY
-import com.example.Ras.Utils.createToast
-import com.example.Ras.Utils.getUserModel
+import com.example.Ras.Utils.*
 import com.example.Ras.models.User
+import com.google.firebase.database.DatabaseReference
 import kotlinx.android.synthetic.main.activity_messenger.*
 import kotlinx.android.synthetic.main.activity_messenger.view.*
 import kotlinx.android.synthetic.main.fragment_single_chat.*
 import kotlinx.android.synthetic.main.toolbar_chat_info.*
 import kotlinx.android.synthetic.main.toolbar_chat_info.view.*
 
-class SingleChatFragment : BaseFragment(R.layout.fragment_single_chat) {
+class SingleChatFragment(private val contact: User) : BaseFragment(R.layout.fragment_single_chat) {
 
     private lateinit var mListenerInfoToolbar: AppValueEventListener
     private lateinit var mReceivingUser: User
     private lateinit var mToolbarInfo: View
+    private lateinit var mRefUser: DatabaseReference
 
     override fun onResume() {
         super.onResume()
@@ -29,14 +28,17 @@ class SingleChatFragment : BaseFragment(R.layout.fragment_single_chat) {
             mReceivingUser = it.getUserModel()
             initInfoToolbar()
         }
+        mRefUser = REF_DATABASE.child(NODE_USERS).child(contact.id)
+        mRefUser.addValueEventListener(mListenerInfoToolbar)
     }
 
     private fun initInfoToolbar() {
-        mToolbarInfo.chat_info_group
+        mToolbarInfo.chat_info_group.text = mReceivingUser.Group
     }
 
     override fun onPause() {
         super.onPause()
         mToolbarInfo.visibility = View.GONE
+        mRefUser.removeEventListener(mListenerInfoToolbar)
     }
 }
